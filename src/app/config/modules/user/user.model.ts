e7176bf3f21cf5a0e763e363d1c41f15d80fe414
Ridwankw <ridwanh.kw@gmail.com>
2023-11-24 14:55:33 +0300

    added some data

---
+++ src/app/config/modules/user/user.model.ts
@@ -0,0 +1,79 @@
+import { Schema, Document, model } from 'mongoose';
+import { Address, Order, User } from './user.interface';
+
+const userAddress = new Schema<Address>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const userOrder = new Schema<Order>({
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const userSchema = new Schema<User & Document>(
+  {
+    userId: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+    hobbies: [{ type: String }],
+    address: userAddress,
+    orders: [userOrder],
+  },
+  { timestamps: true },
+);
+
+const User = model<User>('User', userSchema);
